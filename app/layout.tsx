7fbcfbffd3ffc65c14760c987e0abc130b195ba8
versis <versis791@gmail.com>
2025-05-18 16:04:40 +0200

    removed OG

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,34 +11,7 @@ export const metadata: Metadata = {
   icons: [
     { rel: "icon", url: "/sf-icon.png" },
     { rel: "apple-touch-icon", url: "/sf-icon.png" },
-  ],
-  openGraph: {
-    type: "website",
-    title: "shadefreude - unique color cards (part of tinker.institute)",
-    description: "Create beautiful color reference cards with shadefreude",
-    siteName: "shadefreude",
-    images: [
-      {
-        url: "https://sf.tinker.institute/og?title=shadefreude&description=Create beautiful color reference cards",
-        width: 1200,
-        height: 630,
-        alt: "Shadefreude Card Preview"
-      },
-    ],
-  },
-  twitter: {
-    card: "summary_large_image",
-    title: "shadefreude - unique color cards (part of tinker.institute)",
-    description: "Create beautiful color reference cards with shadefreude",
-    images: [
-      {
-        url: "https://sf.tinker.institute/og?title=Shadefreude&description=Create beautiful color reference cards",
-        width: 1200,
-        height: 630,
-        alt: "Shadefreude Card Preview"
-      },
-    ],
-  },
+  ]
 };
 
 export default function RootLayout({
