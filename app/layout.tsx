3197e16791e1e0e97757228f2289c9a9b37e2d01
versis <versis791@gmail.com>
2025-05-19 20:14:33 +0200

    fixed umami

--- app/layout.tsx
+++ app/layout.tsx
@@ -21,7 +21,13 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <head></head>
+      <head>
+        <script
+          defer
+          src="https://cloud.umami.is/script.js"
+          data-website-id="70281e47-1a9e-4509-8769-fd2e266a4cf5"
+        />
+      </head>
       <body className={cn(GeistSans.className, "antialiased")}>
         <Toaster position="top-center" richColors />
         {children}
