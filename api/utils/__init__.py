c3115038c5ceaceb2cad63075ac0a369aae5364f
versis <versis791@gmail.com>
2025-05-19 23:53:40 +0200

    bug in the code

--- api/utils/__init__.py
+++ api/utils/__init__.py
@@ -3,7 +3,7 @@ API utility modules for the application.
 """
 from api.utils.ai_utils import generate_ai_card_details
 from api.utils.image_processor import resize_and_convert_image_for_openai, ImageProcessor
-from api.utils.openai_client import azure_client, OVERALL_TIMEOUT
+from api.utils.openai_client import azure_client
 from api.utils.response_formatter import OpenAIResponseFormatter
 
 __all__ = [
@@ -11,6 +11,5 @@ __all__ = [
     'resize_and_convert_image_for_openai',
     'ImageProcessor',
     'azure_client',
-    'OVERALL_TIMEOUT',
     'OpenAIResponseFormatter'
 ]
