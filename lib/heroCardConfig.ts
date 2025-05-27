9b3effa413b90b0284d0f52b5eed7c8b71844ad0
versis <versis791@gmail.com>
2025-05-28 00:48:36 +0200

    reordered hero card

--- lib/heroCardConfig.ts
+++ lib/heroCardConfig.ts
@@ -2,8 +2,8 @@
 // This is the single source of truth for which cards appear in the hero section
 
 export const HERO_CARD_IDS = [
-  "000000516 FE F",
   "000000485 FE F",
+  "000000516 FE F",
   "000000475 FE F",
   "000000473 FE F",
   "000000512 FE F",
